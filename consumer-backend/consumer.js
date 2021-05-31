94e06e34370551dcefdaa9b57ce57e738b784ba4
Michael Dawson <mdawson@devrus.com>
2021-05-31 18:04:31 -0400

    feat: update to support SASL_PLAIN
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- consumer-backend/consumer.js
+++ consumer-backend/consumer.js
@@ -23,6 +23,12 @@ try {
   kafkaConnectionBindings = {
     brokers: [process.env.KAFKA_BOOTSTRAP_SERVER || 'my-cluster-kafka-bootstrap:9092']
   };
+  if (process.env.KAFKA_SASL_MECHANISM === 'plain') {
+    kafkaConnectionBindings.sasl =  { mechanism: process.env.KAFKA_SASL_MECHANISM,
+                                      username: process.env.KAFKA_CLIENT_ID,
+                                      password: process.env.KAFKA_CLIENT_SECRET }
+    kafkaConnectionBindings.ssl = true;
+  }
 }
 
 // add the client id
@@ -38,6 +44,7 @@ const run = async () => {
 
   await consumer.run({
     eachMessage: async ({ message }) => {
+      console.log(message.value.toString());
       update(message.value.toString());
     }
   });
--- producer-backend/producer.js
+++ producer-backend/producer.js
@@ -14,6 +14,12 @@ try {
   kafkaConnectionBindings = {
     brokers: [process.env.KAFKA_BOOTSTRAP_SERVER || 'my-cluster-kafka-bootstrap:9092']
   };
+  if (process.env.KAFKA_SASL_MECHANISM === 'plain') {
+    kafkaConnectionBindings.sasl =  { mechanism: process.env.KAFKA_SASL_MECHANISM,
+                                      username: process.env.KAFKA_CLIENT_ID,
+                                      password: process.env.KAFKA_CLIENT_SECRET }
+    kafkaConnectionBindings.ssl = true;
+  }
 }
 
 // add the client id
