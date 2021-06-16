37da0ec9b9988efbc339b0365bc2fe0e5b6b8201
Helio Frota <00hf11@gmail.com>
2021-06-16 16:57:46 -0300

    build: basic integration test

---
+++ scripts/prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#: Title       : Installs producer and consumer
+#: Date        : 2021-06-16
+#: Author      : "Helio Frota" <00hf11 at gmail dot com>
+#: Version     : 0.0.1
+#: Description : Just install the requirements via npm install
+
+cd producer-backend
+npm i
+cd ..
+cd consumer-backend
+npm i
